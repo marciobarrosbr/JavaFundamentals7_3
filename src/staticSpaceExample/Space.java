6ac983a06167d0d6734fe44236f9121ddea648c0
marciobarrosbr <mf.barros84@gmail.com>
2024-01-21 08:13:57 -0300

    parameter correction
    correction on parameters of instance point

--- src/staticSpaceExample/Space.java
+++ src/staticSpaceExample/Space.java
@@ -12,18 +12,18 @@ public class Space {
 			this.z = z;
 		}//end constructor Point
 		
-		//metodo estatico exclusivo para acessar a classe estatica Points e seu construtor
+		//método estatico exclusivo para acessar a classe estatica Points e seu construtor
 		public static Points creatPoint(double x, double y, double z) {
-			return new Points(x,y,x);
+			return new Points(x,y,z);
 		}
 		
 		public void display() {
-			System.out.println("Point: (" + x + "," + y + "," + "," + z + ")");
+			System.out.println("Point: (" + x + "," + y + "," + z + ")");
 		}//end method display
 		
 	}//end static class Point
 	
-	//clase aninhada estática Line
+	//classe aninhada estática Line
 	public static class Line{
 		private Points startPoint, endPoint;//a classe Line possui duas instancias da classe Point como variáveis privadas
 		
@@ -32,11 +32,11 @@ public class Space {
 			this.endPoint = endPoint;
 		}//end constructor Line
 		
-		//metodo estatico exclusivo para acessar a classe estatica Line e seu construtor
+		//método estatico exclusivo para acessar a classe estatica Line e seu construtor
 		public static Line createLine(Points a, Points b) {
 			
 			return new Line(a, b);
-		}//end metodo estatitico Line para acerros exclusivo a classe Line e seu construtor
+		}//end método estatitico Line para acesso exclusivo a classe Line e seu construtor
 		
 		public void display() {
 			System.out.println("Line From: ");
@@ -56,7 +56,7 @@ public class Space {
 			this.sideLength = sideLength;
 		}//end constructor Cube
 		
-		//metodo estatico exclusivo para acessar a classe estatica Cube e seu construtor
+		//método estatico exclusivo para acessar a classe estatica Cube e seu construtor
 		public static Cube creatCube(Points a, double b) {
 			return new Cube(a,b);
 		}//end metodo estatitico Line para acerros exclusivo a classe Cube e seu construtor
--- src/staticSpaceExample/SpaceTester.java
+++ src/staticSpaceExample/SpaceTester.java
@@ -11,9 +11,18 @@ public class SpaceTester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//exemplo de uso das classe aninhadas estáticas
-		Points point = Points.creatPoint(1, 2, 3);
+		Points point = Points.creatPoint(3,2,1);
 		Line line = Line.createLine(Points.creatPoint(0,0,1), Points.creatPoint(2,2,2));
 		Cube cube= Cube.creatCube(Points.creatPoint(1,1,1), 3);
+		
+		System.out.println("Display Point: ");
+		point.display();
+		
+		System.out.println("\nDisplay Line: ");
+		line.display();
+		
+		System.out.println("\nDisplaying Cube");
+		cube.display();
 	}
 
 }
